bb6da983a6a76a17f1ae6d7a2b927c5a4bc8b6eb
ran.yimo <375753904@qq.com>
2026-03-20 23:36:22 +0900

    前端草图

--- frontendForDevelop/EnrichSystem-Front/src/App.vue
+++ frontendForDevelop/EnrichSystem-Front/src/App.vue
@@ -1,20 +1,45 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed, onMounted } from 'vue'
+import axios from 'axios'
+interface TaskItem {
+  id: number
+  title: string
+  type: 'pp' | 'cp'
+}
+const tasks = ref<TaskItem[]>([])
+
+const ppTasks = computed(()=>{
+  return tasks.value.filter(x=>x.type == 'pp')
+})
+
+const cpTasks = computed(()=>{
+  return tasks.value.filter(x=> x.type == 'cp')
+})
+onMounted(async()=>
+{
+  try
+  {
+    //todo:把自己的接口写出来
+    const response = await axios.get('xxx/xxx')
+  }
+  catch(error){
+    //todo:描述换成dnd风格
+    console.error('获取任务失败',error)
+  }
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!123123sd" />
+  <div class="page">
+    <div class="board">
+      <div class = "task-column">
+        <h2>pp任务</h2>
+        <div v-for="task in ppTasks":key="task.id"></div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
