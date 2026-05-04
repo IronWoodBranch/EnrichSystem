cdba1688902160fffc09a314acb6d02671802732
ran.yimo <375753904@qq.com>
2026-05-04 23:58:21 +0900

    周末菜单建模

---
+++ backendforDevelop/EnrichSystem.Infrastructure/DBModels/CookList/CookListEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnrichSystem.Infrastructure.DBModels.CookList
+{
+    public class CookListEntity
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string 
+    }
+}
